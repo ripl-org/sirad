494acc5a92708fa47d4ac2780dc0517023ab8991
Mark Howison <mhowison@ripl.org>
2020-03-09 21:43:11 -0400

    implemented sirad validate command for csv and xlsx

--- sirad/validate.py
+++ sirad/validate.py
@@ -1,26 +1,37 @@
-def ssn(digits):
-    """
-    Clean SSN and check if valid using information from ssa.gov website
-    See : https://www.ssa.gov/employer/stateweb.htm
-    """
-    valid = "0"
-    invalid = "1"
-    # Wrong length
-    if len(digits) != 9:
-        return invalid
-    # Any component is all 0
-    elif digits[:3] == "000" or digits[3:5] == "00" or digits[5:9] == "0000":
-        return invalid
-    # Area 666
-    elif digits[:3] == "666":
-        return invalid
-    # Areas 900-999
-    elif digits.startswith("9"):
-        return invalid
-    # used in an ad by the Social Security Administration
-    elif digits == "219099999":
-        return invalid
-    # Woolworth Wallet Fiasco
-    elif digits == "078051120":
-        return invalid
-    return valid
+"""
+Provides a function to validate a dataset config file for syntax errors
+and against the column names in the raw file.
+"""
+
+import logging
+
+from sirad import config
+from openpyxl import load_workbook
+
+def Validate(dataset):
+
+    logging.info("Validating {}".format(dataset.name))
+    nwarnings = 0
+    firstline = None
+
+    # Read first line of input file
+    if dataset.type == "csv":
+        with open(dataset.source, "r", encoding=dataset.encoding, newline="") as f:
+            firstline = [c.strip().upper() for c in next(f).split(dataset.delimiter)]
+    elif dataset.type == "xlsx":
+        wb = load_workbook(filename=dataset.source, read_only=True, keep_links=False)
+        firstline = [c.value.strip().upper() for c in next(wb.active.rows)]
+        wb.close()
+    if firstline is not None:
+        if dataset.header:
+            firstline = frozenset(firstline)
+            for f in dataset.fields:
+                if f.name.upper() not in firstline:
+                    logging.warn("Dataset {} has no column named {}".format(dataset.name, f.name.upper()))
+                    nwarnings += 1
+        else:
+            if len(firstline) != len(dataset.fields):
+                logging.warn("Dataset {} has {} columns in layout and {} columns in the raw file".format(dataset.name, len(dataset.fields), len(firstline)))
+                nwarnings += 1
+
+    return nwarnings
