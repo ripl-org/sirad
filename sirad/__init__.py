f4e55a4d788840a65878fcd857c35950c66366e9
Mark Howison <mhowison@ripl.org>
2020-03-12 17:04:41 -0400

    extract: improving logging

--- sirad/__init__.py
+++ sirad/__init__.py
@@ -24,6 +24,12 @@ class Log(object):
         self.name = ":".join(names)
         self.log = logging.getLogger(self.name)
 
+    def debug(self, *message, sep=" "):
+        self.log.debug(" {}".format(sep.join(map(str, message))))
+
+    def error(self, *message, sep=" "):
+        self.log.error(" {}".format(sep.join(map(str, message))))
+
     def info(self, *message, sep=" "):
         self.log.info(" {}".format(sep.join(map(str, message))))
 
